7b332c21af58d2033b5fe7e9f276b2c2e17e231d
Win-key <ven2701@gmail.com>
2019-07-07 19:22:46 +0530

    Checking the automatic jenkins build trigger

--- src/main/java/com/winkey/demo/jenkins/controller/JenkinsController.java
+++ src/main/java/com/winkey/demo/jenkins/controller/JenkinsController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JenkinsController {
 
+	//SayHello
 	@GetMapping("/sayHi")
 	public String sayHi() {
 		return "Hie......?";
